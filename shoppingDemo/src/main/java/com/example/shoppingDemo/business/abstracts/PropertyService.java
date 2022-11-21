ebdc0c8e32092681e31e607f37419a3fd26fc4f8
[betulkayam24@gmail.com] <betulkayam24@gmail.com>
2022-11-21 11:09:04 +0300

    Changes by burcu

--- shoppingDemo/src/main/java/com/example/shoppingDemo/business/abstracts/PropertyService.java
+++ shoppingDemo/src/main/java/com/example/shoppingDemo/business/abstracts/PropertyService.java
@@ -1,5 +1,8 @@
 package com.example.shoppingDemo.business.abstracts;
 
+import com.example.shoppingDemo.business.dtos.PropertyDto;
+
 public interface PropertyService {
+    void add(PropertyDto propertyDto);
 
 }
