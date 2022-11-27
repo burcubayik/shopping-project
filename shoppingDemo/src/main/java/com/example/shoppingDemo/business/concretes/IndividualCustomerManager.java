bea4df85d3cbd824e8b867d38fc505d43833e3c1
[betulkayam24@gmail.com] <betulkayam24@gmail.com>
2022-11-27 22:01:08 +0300

    added getAll and getById for IndividualCustomer Service

--- shoppingDemo/src/main/java/com/example/shoppingDemo/business/concretes/IndividualCustomerManager.java
+++ shoppingDemo/src/main/java/com/example/shoppingDemo/business/concretes/IndividualCustomerManager.java
@@ -8,12 +8,20 @@ import com.example.shoppingDemo.business.response.individualCustomers.GetAllIndi
 import com.example.shoppingDemo.business.response.individualCustomers.GetByIndividualCustomerResponse;
 import com.example.shoppingDemo.core.utilities.results.DataResult;
 import com.example.shoppingDemo.core.utilities.results.Result;
+import com.example.shoppingDemo.core.utilities.results.SuccessDataResult;
+import com.example.shoppingDemo.dataAccess.abstracts.IndividualCustomerRepository;
+import com.example.shoppingDemo.entities.concretes.IndividualCustomer;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class IndividualCustomerManager implements IndividualCustomerService {
+    IndividualCustomerRepository individualCustomerRepository;
+    public IndividualCustomerManager(IndividualCustomerRepository individualCustomerRepository){
+        this.individualCustomerRepository=individualCustomerRepository;
+    }
     @Override
     public Result add(CreateIndividualCustomerRequest createIndividualCustomerRequest) {
         return null;
@@ -31,11 +39,30 @@ public class IndividualCustomerManager implements IndividualCustomerService {
 
     @Override
     public DataResult<List<GetAllIndividualCustomersResponse>> getAll() {
-        return null;
+        List<IndividualCustomer> result = this.individualCustomerRepository.findAll();
+        List<GetAllIndividualCustomersResponse> response = result.stream().map(individualCustomer -> GetAllIndividualCustomersResponse
+                .builder()
+                .individualCustomerId(individualCustomer.getIndividualCustomerId())
+                .nationalityId(individualCustomer.getNationalityId())
+                .birthDate(individualCustomer.getBirthDate())
+                .firstName(individualCustomer.getFirstName())
+                .lastName(individualCustomer.getLastName())
+                .gender(individualCustomer.getGender())
+                .build()).collect(Collectors.toList());
+        return new SuccessDataResult<>(response);
     }
 
     @Override
     public DataResult<GetByIndividualCustomerResponse> getById(int individualCustomerId) {
-        return null;
+        IndividualCustomer result = this.individualCustomerRepository.findById(individualCustomerId).get();
+        GetByIndividualCustomerResponse response = GetByIndividualCustomerResponse.builder()
+                .individualCustomerId(result.getIndividualCustomerId())
+                .nationalityId(result.getNationalityId())
+                .birthDate(result.getBirthDate())
+                .firstName(result.getFirstName())
+                .lastName(result.getLastName())
+                .gender(result.getGender())
+                .build();
+        return new SuccessDataResult<>(response);
     }
 }
--- shoppingDemo/src/main/java/com/example/shoppingDemo/business/response/individualCustomers/GetByIndividualCustomerResponse.java
+++ shoppingDemo/src/main/java/com/example/shoppingDemo/business/response/individualCustomers/GetByIndividualCustomerResponse.java
@@ -1,12 +1,21 @@
 package com.example.shoppingDemo.business.response.individualCustomers;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class GetByIndividualCustomerResponse {
     private int individualCustomerId;
+    private String nationalityId;
+    private String firstName;
+    private String lastName;
+    private Date birthDate;
+    private String gender;
 }
