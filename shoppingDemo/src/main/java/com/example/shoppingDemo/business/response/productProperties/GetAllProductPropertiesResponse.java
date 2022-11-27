26cbbc40eaf369fa9f02405516f8da784f152068
[betulkayam24@gmail.com] <betulkayam24@gmail.com>
2022-11-27 22:02:01 +0300

    added getAll and getById for ProductProperty Service

--- shoppingDemo/src/main/java/com/example/shoppingDemo/business/response/productProperties/GetAllProductPropertiesResponse.java
+++ shoppingDemo/src/main/java/com/example/shoppingDemo/business/response/productProperties/GetAllProductPropertiesResponse.java
@@ -1,12 +1,14 @@
 package com.example.shoppingDemo.business.response.productProperties;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class GetAllProductPropertiesResponse {
     private int id;
     private int productId;
--- shoppingDemo/src/main/java/com/example/shoppingDemo/business/response/productProperties/GetByProductPropertyResponse.java
+++ shoppingDemo/src/main/java/com/example/shoppingDemo/business/response/productProperties/GetByProductPropertyResponse.java
@@ -1,12 +1,16 @@
 package com.example.shoppingDemo.business.response.productProperties;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class GetByProductPropertyResponse {
     private int id;
+    private int productId;
+    private int propertyId;
 }
